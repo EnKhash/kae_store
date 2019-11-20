10f5d87e48f4dfaafc7edad2a35d53718c612bb0
kepposo <khaledmaddah1995@gmail.com>
2019-11-21 00:27:19 +0300

    implemented flask

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask, Session, render_template, url_for, request
+from flask import Flask, session, render_template, url_for, request
 
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/hello")
-def hello():
-    return "Hello World!"
+@app.route("/home")
+@app.route("/index")
+def index():
+    return render_template('index.html', title='Home')
+
+@app.route("/registration")
+def registration():
+    return render_template('registration.html', title='Registration')
+
+@app.route("/about")
+def about():
+    return render_template('about_us.html', title='About Us')
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html', title='Contact Us')
 
 app.run(debug= True)
