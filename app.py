068463658af8379c89c897fc8bf99533fe864915
MadLed <khaledmaddah1995@gmail.com>
2019-11-25 01:21:42 +0300

    adgustments

--- app.py
+++ app.py
@@ -32,15 +32,10 @@ with open('products.json') as f:
 #sort items by boughtNum and store in popItems
 popItems = sorted(items, key= lambda i: i['boughtNum'], reverse=True)
 
-
-#write popItems to popular-products.json
-with open('popular-products.json', 'w') as p:
-    json.dump(popItems, p)
-
 #item-discription page
 @app.route("/item-description/<int:item_index>")
 def itemDesc(item_index):
-    return render_template('item-description.html', presented_item= items[item_index] )
+    return render_template('item-description.html', presented_items= items[item_index], title=items[item_index]['name'])
 
 #load product.json file
 with open('products.json') as f:
