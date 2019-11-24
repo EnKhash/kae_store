f589d503dc21390108f85d0f491f885f3f1c8971
MadLed <khaledmaddah1995@gmail.com>
2019-11-25 02:51:39 +0300

    fixed popular items discriptions in index

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route("/home")
 @app.route("/index")
 def index():
-    return render_template('index.html', title='Home' , items=sorted(items, key= lambda i: i['boughtNum'], reverse=True))
+    return render_template('index.html', title='Home', items=popItems)
 
 #Registrarion page
 @app.route("/registration")
@@ -34,8 +34,10 @@ popItems = sorted(items, key= lambda i: i['boughtNum'], reverse=True)
 
 #item-discription page
 @app.route("/item-description/<int:item_index>")
-def itemDesc(item_index):
-    return render_template('item-description.html', presented_items= items[item_index], title=items[item_index]['name'])
+def itemDescPop(item_index):
+    return render_template('item-description.html', presented_items= popItems[item_index], title=popItems[item_index]['name'])
+
+
 
 #load product.json file
 with open('products.json') as f:
