31687ee804ac51a48fec349d4f73caae39305e5c
MadLed <khaledmaddah1995@gmail.com>
2019-11-25 18:24:14 +0300

    finished all of backend

--- app.py
+++ app.py
@@ -2,13 +2,17 @@ from flask import Flask, session, render_template, url_for, request, json
 
 app = Flask(__name__)
 
+#error 404 page
+@app.errorhandler(404)
+def error(error):
+    return render_template('error.html', title='Error 404'), 404
 
 #index page
 @app.route("/")
 @app.route("/home")
 @app.route("/index")
 def index():
-    return render_template('index.html', title='Home', items=popItems)
+    return render_template('index.html', title='Home', items=popular_items(items), item_koko=items)
 
 #Registrarion page
 @app.route("/registration")
@@ -29,18 +33,44 @@ def contact():
 with open('products.json') as f:
   items = json.load(f)
 
+
 #sort items by boughtNum and store in popItems
 popItems = sorted(items, key= lambda i: i['boughtNum'], reverse=True)
 
 #item-discription page
-@app.route("/item-description/<int:item_index>")
-def itemDescPop(item_index):
-    return render_template('item-description.html', presented_items= popItems[item_index], title=popItems[item_index]['name'])
+@app.route("/item-description/<name>/<price>/<desc>")
+def itemDesc(name, price, desc):
+    return render_template('item-description.html', name=name, price=price, desc=desc, title=name)
 
 
+#item list page
+@app.route('/item-list/<string:item_type>')
+def item_list(item_type):
+    return render_template('item-list.html', item_type=find_type(items, item_type), title=item_type + ' items')
 
-#load product.json file
-with open('products.json') as f:
-  items = json.load(f)
+def popular_items(items):
+    for i in range(0,9):
+        for j in range(i+1, len(items)):
+            if items[i]['boughtNum'] < items[j]['boughtNum']:
+                temp = items[i]
+                items[i] = items[j]
+                items[j] = temp
+        yield items[i]
+        json.dumps(items[i])
+        
+
+def find_type(items, itype):
+    for i in range(0,len(items)):
+        if(items[i]['type'] == 'PS4' and itype == "PS4"):
+            yield items[i]
+            json.dumps(items[i])
+        elif(items[i]['type'] == 'Xbox' and itype == "Xbox"):
+            yield items[i]
+            json.dumps(items[i])
+        elif(items[i]['type'] == 'PC' and itype == "PC"):
+            yield items[i]
+            json.dumps(items[i])
+
+
 
 app.run(debug= True)
