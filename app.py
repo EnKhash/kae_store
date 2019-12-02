a7cb517501c9a60699f7816cadc7f3e26965a5e4
EnKhash <EnKhash@users.noreply.github.com>
2019-12-02 09:27:05 +0300

    uploding again

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask_mail import Mail, Message
 app = Flask(__name__)
 
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-app.config['MAIL_PORT'] = 587
+app.config['MAIL_PORT'] = Process.env.PORT || 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'khaledmaddah1995@gmail.com'
 app.config['MAIL_PASSWORD'] = 'remfzluosfocpopa'
